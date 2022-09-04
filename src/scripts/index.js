0ccd5c8b9b10a32883d2d8742053ef387df36b78
Robin Verona <robin.anorev@proton.me>
2022-09-04 18:44:07 +0200

    fix(sidebar): update sidebar to fix scroll problem & toggle

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,16 +4,18 @@ console.log('Hello World');
 const mobileMenu = document.querySelector('.sidebar');
 const mobileMenuButton = document.querySelector('#sidebar__button--open');
 const mobileCloseMenuButton = document.querySelector('#sidebar-header__button--close');
-console.log(mobileCloseMenuButton);
+console.log(mobileMenu);
 
 mobileMenuButton.addEventListener('click', () => {
     mobileMenu.classList.remove('sidebar--hidden');
-
     mobileMenu.classList.add('sidebar--open');
+    document.querySelector('body').style.position = 'fixed';
+    console.log(mobileMenu.classList);
 });
 
   mobileCloseMenuButton.addEventListener('click', () => {
     mobileMenu.classList.remove('sidebar--open');
-
     mobileMenu.classList.add('sidebar--hidden');
+    document.querySelector('body').style.position = 'static';
+    console.log(mobileMenu.classList);
   })
