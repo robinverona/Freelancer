9f247763c24a4043a9b51dc62b8124be0c524589
Robin Verona <robin.anorev@proton.me>
2022-09-04 12:34:09 +0200

    feat(global): import js file && fix styles issues

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
 console.log('Hello World');
+
+
+const mobileMenu = document.querySelector('.sidebar');
+const mobileMenuButton = document.querySelector('#sidebar__button--open');
+const mobileCloseMenuButton = document.querySelector('#sidebar-header__button--close');
+console.log(mobileCloseMenuButton);
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.remove('sidebar--hidden');
+
+    mobileMenu.classList.add('sidebar--open');
+});
+
+  mobileCloseMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.remove('sidebar--open');
+
+    mobileMenu.classList.add('sidebar--hidden');
+  })
